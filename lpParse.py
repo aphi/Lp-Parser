a707878c37653e39cf4c6df311bdfd3531b33f3b
Antony Phillips <antony.phillips@auckland.ac.nz>
2012-12-20 21:00:44 +1300

    Finished generic matrix output standard, and fixed parse bugs

--- lpParse.py
+++ lpParse.py
@@ -3,13 +3,28 @@
 # work out elegant error handling
 # focus on output objects, and document clearly
 
+# output pulp.LpProblem instance
+# need to generate the LpVariable objects, form constraints and objectives, and add them to an LpProblem object
+# variables are not directly added to the LpProblem
+
 # ideally we would shift any constraints from the A matrix which only pertain to one variable into the var bounds section
 
 from pyparsing import *
-from sys import argv
+from sys import argv, exit
+from lpParse_f import Matrix, multiRemove
+import constants
 
-from ordereddict import OrderedDict as odict
-from lpParse_f import Matrix, multiRemove, setBounds
+# read input lp file
+try:
+    fp = open(argv[1])
+    fullDataString = fp.read()
+    fp.close()
+except IndexError:
+    print "Usage: $ python lpParse.py <lpfile>"
+    exit()
+except IOError:
+    print "Could not find input lp file \"%s\""%argv[1]
+    exit()
 
 #name char ranges for objective, constraint or variable
 allNameChars = alphanums + "!\"#$%&()/,.;?@_'`{}|~"
@@ -29,8 +44,7 @@ sense = oneOf("< <= =< = > >= =>").setResultsName("sense")
 # section tags
 objTagMax = oneOf("max maximum maximize", caseless=True)
 objTagMin = oneOf("min minimum minimize", caseless=True)
-objTag = objTagMax | objTagMin
-objTag.setResultsName("objSense")
+objTag = (objTagMax | objTagMin).setResultsName("objSense")
 
 constraintsTag = oneOf(["subj to", "subject to", "s.t.", "st"], caseless=True)
 
@@ -56,7 +70,7 @@ varExpr = firstVar + ZeroOrMore(var)
 varExpr = varExpr.setResultsName("varExpr")
 
 # objective
-objective = Optional(validName + colon) + varExpr
+objective = objTag + Optional(validName + colon) + varExpr
 objective = objective.setResultsName("objective")
 
 # constraint rhs
@@ -70,8 +84,11 @@ constraints = ZeroOrMore(constraint)
 constraints = constraints.setResultsName("constraints")
 
 # bounds
-numberOrInf = number | Combine(plusMinus + inf)
-sensestmt = Group(Optional(numberOrInf + sense) + validName + Optional(sense + numberOrInf))
+signedInf = (plusMinus + inf).setParseAction(lambda tokens:(tokens[0] == "+") * constants.infinity)
+signedNumber = (Optional(plusMinus, "+") + number).setParseAction(lambda tokens: eval("".join(tokens)))  # this is different to previous, because "number" is mandatory not optional
+numberOrInf = (signedNumber | signedInf).setResultsName("numberOrInf")
+ineq = numberOrInf & sense
+sensestmt = Group(Optional(ineq).setResultsName("leftbound") + validName + Optional(ineq).setResultsName("rightbound"))
 freeVar = Group(validName + Literal("free"))
 
 boundstmt = freeVar | sensestmt 
@@ -85,83 +102,16 @@ binaries = binTag + ZeroOrMore(validName).setResultsName("binaries")
 
 varInfo = ZeroOrMore(bounds | generals | binaries)
 
-grammar = objTag + objective + constraintsTag + constraints + varInfo + endTag
+grammar = objective + constraintsTag + constraints + varInfo + endTag
 
 # commenting
 commentStyle = Literal("\\") + restOfLine
 grammar.ignore(commentStyle)
 
-fp = open(argv[1])
-fullDataString = fp.read()
-fp.close()
+# parse input string
+parseOutput = grammar.parseString(fullDataString)
 
-full = grammar.parseString(fullDataString)
+# create generic output Matrix object
+m = Matrix(parseOutput.objective, parseOutput.constraints, parseOutput.bounds, parseOutput.generals, parseOutput.binaries)
 
-#print full
-
-outputObjective = {}
-
-o = full.objective
-print "Objective: %s"%o.objSense
-print o.name, o.varExpr
-outputObjective["sense"] = o.objSense
-outputObjective["name"] = o.name
-outputObjective["expression"] = {}
-for v in o.varExpr:
-    outputObjective["expression"][v.name] = v.coef 
-
-outputConstraints = {}
-
-senseLiterals = {"<":"le", "<=":"le", "=<":"le", "=":"eq", ">":"ge", ">=":"ge", "=>":"ge"}
-
-print "\nConstraints"
-for c in full.constraints:
-    outputConstraints[c.name] = {}
-    outputConstraints[c.name]["name"] = c.name
-    outputConstraints[c.name]["rhs"] = c.rhs
-    outputConstraints[c.name]["sense"] = senseLiterals[sense]
-    outputConstraints[c.name]["expression"] = {}
-    for v in c.varExpr:
-        outputConstraints[c.name]["expression"][v.name] = v.coef
-
-outputVarRanges = {}
-print "\nBounds"
-for b in full.bounds:
-    if len(b) == 2: # free var
-        outputVarRanges[b[0]] = {"lb":None, "ub":None}
-        
-    elif len(b) == 3: # one sided ineq
-
-        setBounds(outputVarRanges, b)
-        sense = senseLiterals[b[1]]
-
-        if hasattr(b[0], "name"):
-            if sense == "le": # v <= b[2]
-                outputVarRanges[b[0]] = {"lb":None, "ub":b[2]}
-            elif sense == "ge": # v >= b[2]
-                outputVarRanges[b[0]] = {"lb":b[2], "ub":None}
-            else: # v == b[2]
-                outputVarRanges[b[0]] = {"lb":b[2], "ub":b[2]}
-
-        elif hasattr(b[2], "name"):
-            if sense == "le": # b[0] <= v
-                outputVarRanges[b[2]] = {"lb":b[0], "ub":None}
-            elif sense == "ge": # b[0] >= v
-                outputVarRanges[b[2]] = {"lb":None, "ub":b[0]}
-            else: # b[0] == v
-                outputVarRanges[b[2]] = {"lb":b[0], "ub":b[0]}
-        
-    
-    elif len(b) == 5: # two sided ineq
-        
-        outputVarRanges[b[2]] = {"lb": , "ub":}
-
-
-print "\nGenerals"
-for g in full.generals:
-    print g
-
-print "\nBinaries"
-for b in full.binaries:
-    print b
-
+print m
