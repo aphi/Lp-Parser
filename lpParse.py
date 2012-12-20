654609c45fb2d662a1c4197c3d07029834f85eb5
Antony Phillips <antony.phillips@auckland.ac.nz>
2012-12-20 21:13:53 +1300

    Updated doc and usage capabilities

--- lpParse.py
+++ lpParse.py
@@ -14,104 +14,113 @@ from sys import argv, exit
 from lpParse_f import Matrix, multiRemove
 import constants
 
-# read input lp file
-try:
-    fp = open(argv[1])
-    fullDataString = fp.read()
-    fp.close()
-except IndexError:
-    print "Usage: $ python lpParse.py <lpfile>"
-    exit()
-except IOError:
-    print "Could not find input lp file \"%s\""%argv[1]
-    exit()
+def read(filename):
+    # read input lp file
+    try:
+        fp = open(filename)
+        fullDataString = fp.read()
+        fp.close()
+    except IOError:
+        print "Could not find input lp file \"%s\""%argv[1]
+        raise IOError
 
-#name char ranges for objective, constraint or variable
-allNameChars = alphanums + "!\"#$%&()/,.;?@_'`{}|~"
-firstChar = multiRemove(allNameChars, nums + "eE.") #<- can probably use CharsNotIn instead
-name = Word(firstChar, allNameChars, max=255)
-keywords = ["inf", "infinity", "max", "maximum", "maximize", "min", "minimum", "minimize", "s.t.", "st", "bound", "bounds", "bin", "binaries", "binary", "gen",  "general", "end"]
-pyKeyword = MatchFirst(map(CaselessKeyword, keywords))
-validName = ~pyKeyword + name
-validName = validName.setResultsName("name")
 
-colon = Suppress(oneOf(": ::"))
-plusMinus = oneOf("+ -")
-inf = oneOf("inf infinity", caseless=True)
-number = Word(nums+".")
-sense = oneOf("< <= =< = > >= =>").setResultsName("sense")
+    #name char ranges for objective, constraint or variable
+    allNameChars = alphanums + "!\"#$%&()/,.;?@_'`{}|~"
+    firstChar = multiRemove(allNameChars, nums + "eE.") #<- can probably use CharsNotIn instead
+    name = Word(firstChar, allNameChars, max=255)
+    keywords = ["inf", "infinity", "max", "maximum", "maximize", "min", "minimum", "minimize", "s.t.", "st", "bound", "bounds", "bin", "binaries", "binary", "gen",  "general", "end"]
+    pyKeyword = MatchFirst(map(CaselessKeyword, keywords))
+    validName = ~pyKeyword + name
+    validName = validName.setResultsName("name")
 
-# section tags
-objTagMax = oneOf("max maximum maximize", caseless=True)
-objTagMin = oneOf("min minimum minimize", caseless=True)
-objTag = (objTagMax | objTagMin).setResultsName("objSense")
+    colon = Suppress(oneOf(": ::"))
+    plusMinus = oneOf("+ -")
+    inf = oneOf("inf infinity", caseless=True)
+    number = Word(nums+".")
+    sense = oneOf("< <= =< = > >= =>").setResultsName("sense")
 
-constraintsTag = oneOf(["subj to", "subject to", "s.t.", "st"], caseless=True)
+    # section tags
+    objTagMax = oneOf("max maximum maximize", caseless=True)
+    objTagMin = oneOf("min minimum minimize", caseless=True)
+    objTag = (objTagMax | objTagMin).setResultsName("objSense")
 
-boundsTag = oneOf("bound bounds", caseless=True)
-binTag = oneOf("bin binaries binary", caseless=True)
-genTag = oneOf("gen general", caseless=True)
+    constraintsTag = oneOf(["subj to", "subject to", "s.t.", "st"], caseless=True)
 
-endTag = CaselessLiteral("end")
+    boundsTag = oneOf("bound bounds", caseless=True)
+    binTag = oneOf("bin binaries binary", caseless=True)
+    genTag = oneOf("gen general", caseless=True)
 
-# coefficient on a variable (includes sign)
-firstVarCoef = Optional(plusMinus, "+") + Optional(number, "1")
-firstVarCoef.setParseAction(lambda tokens: eval("".join(tokens))) #TODO: can't this just be eval(tokens[0] + tokens[1])?
+    endTag = CaselessLiteral("end")
 
-coef = plusMinus + Optional(number, "1")
-coef.setParseAction(lambda tokens: eval("".join(tokens))) #TODO: can't this just be eval(tokens[0] + tokens[1])?
+    # coefficient on a variable (includes sign)
+    firstVarCoef = Optional(plusMinus, "+") + Optional(number, "1")
+    firstVarCoef.setParseAction(lambda tokens: eval("".join(tokens))) #TODO: can't this just be eval(tokens[0] + tokens[1])?
 
-# variable (coefficient and name)
-firstVar = Group(firstVarCoef.setResultsName("coef") + validName)
-var = Group(coef.setResultsName("coef") + validName)
+    coef = plusMinus + Optional(number, "1")
+    coef.setParseAction(lambda tokens: eval("".join(tokens))) #TODO: can't this just be eval(tokens[0] + tokens[1])?
 
-# expression
-varExpr = firstVar + ZeroOrMore(var)
-varExpr = varExpr.setResultsName("varExpr")
+    # variable (coefficient and name)
+    firstVar = Group(firstVarCoef.setResultsName("coef") + validName)
+    var = Group(coef.setResultsName("coef") + validName)
 
-# objective
-objective = objTag + Optional(validName + colon) + varExpr
-objective = objective.setResultsName("objective")
+    # expression
+    varExpr = firstVar + ZeroOrMore(var)
+    varExpr = varExpr.setResultsName("varExpr")
 
-# constraint rhs
-rhs = Optional(plusMinus, "+") + number
-rhs = rhs.setResultsName("rhs")
-rhs.setParseAction(lambda tokens: eval("".join(tokens)))
+    # objective
+    objective = objTag + Optional(validName + colon) + varExpr
+    objective = objective.setResultsName("objective")
 
-# constraints
-constraint = Group(Optional(validName + colon) + varExpr + sense + rhs)
-constraints = ZeroOrMore(constraint)
-constraints = constraints.setResultsName("constraints")
+    # constraint rhs
+    rhs = Optional(plusMinus, "+") + number
+    rhs = rhs.setResultsName("rhs")
+    rhs.setParseAction(lambda tokens: eval("".join(tokens)))
 
-# bounds
-signedInf = (plusMinus + inf).setParseAction(lambda tokens:(tokens[0] == "+") * constants.infinity)
-signedNumber = (Optional(plusMinus, "+") + number).setParseAction(lambda tokens: eval("".join(tokens)))  # this is different to previous, because "number" is mandatory not optional
-numberOrInf = (signedNumber | signedInf).setResultsName("numberOrInf")
-ineq = numberOrInf & sense
-sensestmt = Group(Optional(ineq).setResultsName("leftbound") + validName + Optional(ineq).setResultsName("rightbound"))
-freeVar = Group(validName + Literal("free"))
+    # constraints
+    constraint = Group(Optional(validName + colon) + varExpr + sense + rhs)
+    constraints = ZeroOrMore(constraint)
+    constraints = constraints.setResultsName("constraints")
 
-boundstmt = freeVar | sensestmt 
-bounds = boundsTag + ZeroOrMore(boundstmt).setResultsName("bounds")
+    # bounds
+    signedInf = (plusMinus + inf).setParseAction(lambda tokens:(tokens[0] == "+") * constants.infinity)
+    signedNumber = (Optional(plusMinus, "+") + number).setParseAction(lambda tokens: eval("".join(tokens)))  # this is different to previous, because "number" is mandatory not optional
+    numberOrInf = (signedNumber | signedInf).setResultsName("numberOrInf")
+    ineq = numberOrInf & sense
+    sensestmt = Group(Optional(ineq).setResultsName("leftbound") + validName + Optional(ineq).setResultsName("rightbound"))
+    freeVar = Group(validName + Literal("free"))
 
-# generals
-generals = genTag + ZeroOrMore(validName).setResultsName("generals") 
+    boundstmt = freeVar | sensestmt 
+    bounds = boundsTag + ZeroOrMore(boundstmt).setResultsName("bounds")
 
-# binaries
-binaries = binTag + ZeroOrMore(validName).setResultsName("binaries")
+    # generals
+    generals = genTag + ZeroOrMore(validName).setResultsName("generals") 
 
-varInfo = ZeroOrMore(bounds | generals | binaries)
+    # binaries
+    binaries = binTag + ZeroOrMore(validName).setResultsName("binaries")
 
-grammar = objective + constraintsTag + constraints + varInfo + endTag
+    varInfo = ZeroOrMore(bounds | generals | binaries)
 
-# commenting
-commentStyle = Literal("\\") + restOfLine
-grammar.ignore(commentStyle)
+    grammar = objective + constraintsTag + constraints + varInfo + endTag
 
-# parse input string
-parseOutput = grammar.parseString(fullDataString)
+    # commenting
+    commentStyle = Literal("\\") + restOfLine
+    grammar.ignore(commentStyle)
 
-# create generic output Matrix object
-m = Matrix(parseOutput.objective, parseOutput.constraints, parseOutput.bounds, parseOutput.generals, parseOutput.binaries)
+    # parse input string
+    parseOutput = grammar.parseString(fullDataString)
 
-print m
+    # create generic output Matrix object
+    m = Matrix(parseOutput.objective, parseOutput.constraints, parseOutput.bounds, parseOutput.generals, parseOutput.binaries)
+
+    return m
+
+if __name__ == "__main__":
+    try:
+        argv[1]
+    except IndexError:
+        print "Usage: $ python lpParse.py <lpfile>"
+        exit()
+            
+    m = read(argv[1])
+    print m
